c91d12c65d3581dd9d52351be54d0b0e61f883f0
Wagner Guedes <97996373+wagner-guedes-dev@users.noreply.github.com>
2023-02-25 19:58:59 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function Home() {
                             setBtnCashout(false)
                           }, 800);
                         
-                        return
+
                         
                     }
                     if (cashoutEnable) {
